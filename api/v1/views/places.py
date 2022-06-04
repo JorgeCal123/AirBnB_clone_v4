c5cedd8b8aa48b253589349df1680dea550ca454
JorgeCal123/boom.git <git@github.com>
2022-06-04 14:52:36 +0000

    trash

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -93,6 +93,7 @@ def post_place(city_id):
     return make_response(jsonify(instance.to_dict()), 201)
 
 
+
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
 @swag_from('documentation/place/put_place.yml', methods=['PUT'])
 def put_place(place_id):
