25b311a342d3320771f1f01e67da336acbc9c0e2
ruizdani301 <ruizdani301@gmail.com>
2022-06-04 10:31:41 -0500

    all changes

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -31,20 +31,22 @@ $(document).ready(function () {
 		for (let i = 0; i < data.length; i++) {
 			let place = data[i];
 			$('.places ').append(
-								`<article>
-									<div class="title_box">
-									   <h2> + place.name + </h2>
-									   <div class="price_by_night">$ + place.price_by_night + </div>
-									</div>
-									<div class="information">
-									   <div class="max_guest">$ + place.max_guest + ${place.max_guest !== 1 ? 'Guests' : 'Guest'</div>
-									   <div class="number_rooms">$ + place.number_rooms + ${place.number_rooms !== 1 ? 'Bedrooms' : 'Bedroom'}</div>
-									   <div class="number_bathrooms">${place.number_rooms} ${place.number_rooms !== 1 ? 'Bedrooms' : 'Bedroom'}</div>
-									</div>
-									<div class="user">
-										<b>Owner:</b> + place.user.first_name + place.user.last_name 
-									</div>
-									<div class="description">$ + place.description + </div>
-								 </article>`;
-								}
-});
+								"<article>
+								<div class="title_box">
+								   <h2> ${ place.name } </h2>
+								   <div class="price_by_night">$ { place.price_by_night }</div>
+								</div>
+								<div class="information">
+								   <div class="max_guest">${ place.max_guest } ${ place.max_guest } !== 1 ? 'Guests' : 'Guest'</div>
+								   <div class="number_rooms">${ place.number_rooms }  ${place.number_rooms } !== 1 ? 'Bedrooms' : 'Bedroom'</div>
+								   <div class="number_bathrooms">${place.number_rooms} ${place.number_rooms } !== 1 ? 'Bedrooms' : 'Bedroom'</div>
+								</div>
+								<div class="user">
+									<b>Owner:</b> ${ place.user.first_name } ${ place.user.last_name }
+								</div>
+								<div class="description">$ { place.description }</div>
+								</article>"
+								);
+		}
+	}});
+						});
